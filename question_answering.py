b00c980b629c75a6a2b3967939e4caced5765887
Zimina, Kristina <kristina.zimina21@imperial.ac.uk>
2024-01-30 17:15:01 +0000

    Delete new endpoints

--- question_answering.py
+++ question_answering.py
@@ -11,25 +11,3 @@ def answer_question_from_context(context, question):
     res = qa_pipeline(input)['answer']
 
     return res
-
-print(answer_question_from_context(
-    "work in a school with student of ages 11 - 16 (secondary school) \
-teaching range of stem subjects \
-(ass) permenant position \
-(good at mario kart) \
-experience working with children especially early teenagers \
-strong at math, lots of experience with computer programming \
-learning entirely stem subject for the past 6 years (and doing longer than that) \
-i am personable, good at handling work place conflicts \
-(soft wishes) \
-in london area \
-preferably close to public transport \
-school with good reviews (ofsted schools) \
-reasonably good pay \
-\
-(day-to-day) \
-going in every week day \
-teaching variety of stem subject (math science computing etc) \
-lunch duty, monitoring the general community behaviour and welfare (ex timetable ) \
-making lessons, and marking work, with enough reason to have time for yourself in the evening",
-"What job is suggested?"))

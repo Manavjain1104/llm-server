d10ef5c5e3bf8ac231e55eaf69db3d7b5a397692
Max Stupple <>
2024-02-02 13:34:04 +0000

    Add error handling to Q&A API

--- question_answering.py
+++ question_answering.py
@@ -2,12 +2,17 @@ from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 
 answerer = "deepset/roberta-base-squad2"
 
-def answer_question_from_context(context, question):
-    qa_pipeline = pipeline('question-answering', model=answerer, tokenizer=answerer)
-    input = {
-        'question': question,
-        'context': context
-    }
-    res = qa_pipeline(input)['answer']
+class CreateException(Exception):
+    pass
 
-    return res
+def answer_question_from_context(context, question):
+    try:
+        qa_pipeline = pipeline('question-answering', model=answerer, tokenizer=answerer)
+        input = {
+            'question': question,
+            'context': context
+        }
+        res = qa_pipeline(input)['answer']
+        return res
+    except Exception as e:
+        raise CreateException("Error whilst querying Q&A model.\n" + str(e))

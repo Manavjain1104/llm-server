b2c4e601aeb62304568437133ccedd3add5f1ad4
Zimina, Kristina <kristina.zimina21@imperial.ac.uk>
2024-02-06 11:02:13 +0000

    Update question_answering.py

--- question_answering.py
+++ question_answering.py
@@ -1,29 +1,14 @@
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 
-answerer = pipeline('question-answering', model="deepset/roberta-base-squad2", tokenizer="deepset/roberta-base-squad2")
-
-
-class QuestionException(Exception):
-    pass
-
+answerer = "deepset/roberta-base-squad2"
 
 def answer_question_from_context(context, question):
-    try:
-        print("Begin answering question")
-        print(context)
-        print(question)
-        input = {
-            'question': question,
-            'context': context
-        }
-        print(input)
-        res_no_a = pipeline(input)
-        print(res_no_a)
-        res = res_no_a['answer']
-        return res
-    except KeyboardInterrupt:
-        print("Interrupted")
-        return "Interrupted"
-    except BaseException as e:
-        print("Error")
-        raise QuestionException("Error whilst querying Q&A model.\n" + str(e))
+    qa_pipeline = pipeline('question-answering', model=answerer, tokenizer=answerer)
+    input = {
+        'question': question,
+        'context': context
+    }
+    print(input)
+    res = qa_pipeline(input)['answer']
+
+    return res
